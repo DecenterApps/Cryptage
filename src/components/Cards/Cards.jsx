7d1b517e280c593601f6b05c881cfed7b9dff45b
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-28 19:51:35 +0100

    Add loading and empty states for cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { usersCardsFetch } from '../../actions/appActions';
 import HandCard from './HandCard/HandCard';
 import DragWrapper from '../DragWrapper/DragWrapper';
+import Spinner from '../Spinner/Spinner';
 import { addLocation } from '../../actions/locationActions';
 
 import './Cards.scss';
@@ -14,31 +15,51 @@ class Cards extends Component {
   }
 
   render() {
+    const { cardsFetching, cards } = this.props;
+    const { addLocation } = this.props;
+
     return (
       <div className="cards-wrapper">
-        { this.props.cardsFetching && <div>Fetching cards...</div> }
-        { this.props.cards.map((card, index) => (
-          <div key={card.id} className="card-container">
-            {
-              card.stats.typeIndex === 2 &&
-              <div
-                className="location-card-wrapper"
-                onClick={() => { this.props.addLocation(index, this.props.cards); }}
-              >
-                <HandCard card={card} />
-              </div>
-            }
-
-            {
-              card.stats.typeIndex !== 2 &&
-              <div className="asset-wrapper">
-                <DragWrapper key={card.id} {...{ card }}>
-                  <HandCard card={card} />
-                </DragWrapper>
-              </div>
-            }
+        {
+          cardsFetching &&
+          <div className="loading-cards">
+            <span>Fetching cards...</span>
+            <Spinner color="black" size={4} />
           </div>
-        )) }
+        }
+
+        {
+          !cardsFetching && cards.length === 0 &&
+          <div className="no-cards">
+            You currently do not own any cards.
+          </div>
+        }
+
+        {
+          !cardsFetching && cards.length > 0 &&
+          cards.map((card, index) => (
+            <div key={card.id} className="card-container">
+              {
+                card.stats.typeIndex === 2 &&
+                <div
+                  className="location-card-wrapper"
+                  onClick={() => { addLocation(index, cards); }}
+                >
+                  <HandCard card={card} />
+                </div>
+              }
+
+              {
+                card.stats.typeIndex !== 2 &&
+                <div className="asset-wrapper">
+                  <DragWrapper key={card.id} {...{ card }}>
+                    <HandCard card={card} />
+                  </DragWrapper>
+                </div>
+              }
+            </div>
+          ))
+        }
       </div>
     );
   }
