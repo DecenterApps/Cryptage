0c62ae4e8a0ed2388369615408000657e9fefa88
astankovic <aleksa.stankovic@berkeley.edu>
2018-04-03 11:26:46 +0200

    Add fade in and fade out of fpb for dropped mining cards

--- src/components/ContainerItem/ContainerItem.jsx
+++ src/components/ContainerItem/ContainerItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import HandCard from '../Cards/HandCard/HandCard';
@@ -6,20 +6,50 @@ import HandCard from '../Cards/HandCard/HandCard';
 import './ContainerItem.scss';
 import { handleCardCancel } from '../../actions/gameplayActions';
 
-const ContainerItem = ({ index, cards, locationIndex, containerIndex, slot, handleCardCancel }) => (
-  <div className="container-item-wrapper">
-    <HandCard
-      showCount={false}
-      played
-      card={cards[0]}
-      slot={slot}
-      handleCardCancel={handleCardCancel}
-      locationIndex={locationIndex}
-      containerIndex={containerIndex}
-      containerSlotIndex={index}
-    />
-  </div>
-);
+class ContainerItem extends Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+
+    this.toggleFundsStat = this.toggleFundsStat.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.blockNumber === this.props.blockNumber) return;
+
+    this.toggleFundsStat();
+    setTimeout(this.toggleFundsStat, 2000);
+  }
+
+  toggleFundsStat() {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    const {
+      index, cards, locationIndex, containerIndex, slot, handleCardCancel,
+    } = this.props;
+
+    return (
+      <div className="container-item-wrapper">
+        {
+          this.state.show && <div className="fpb">+ { cards[0].stats.bonus.funds }</div>
+        }
+
+        <HandCard
+          showCount={false}
+          played
+          card={cards[0]}
+          slot={slot}
+          handleCardCancel={handleCardCancel}
+          locationIndex={locationIndex}
+          containerIndex={containerIndex}
+          containerSlotIndex={index}
+        />
+      </div>
+    );
+  }
+}
 
 ContainerItem.defaultProps = {
   cards: [],
@@ -32,11 +62,13 @@ ContainerItem.propTypes = {
   slot: PropTypes.object.isRequired,
   handleCardCancel: PropTypes.func.isRequired,
   index: PropTypes.number.isRequired,
+  blockNumber: PropTypes.number.isRequired,
 };
 
-const mapStateToProps = ({ gameplay }) => ({
+const mapStateToProps = ({ gameplay, app }) => ({
   projects: gameplay.projects,
   activeLocationIndex: gameplay.activeLocationIndex,
+  blockNumber: app.blockNumber,
 });
 
 const mapDispatchToProp = {
