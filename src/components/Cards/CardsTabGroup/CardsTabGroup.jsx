21bdcb7f790a778cb16363b852a1d44e0ffd27a1
madjarevicn <nikola.madjarevic@gmail.com>
2018-04-19 12:48:28 +0200

    Merge containers and miners tab

---
+++ src/components/Cards/CardsTabGroup/CardsTabGroup.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import HandCard from '../HandCard/HandCard';
+import DragWrapper from '../../DragWrapper/DragWrapper';
+
+const CardsTabGroup = ({ type }) => (
+  <div className="card-type-wrapper">
+    <div className="card-type-title-wrapper">
+      <h1 className="card-type-title">{type[0].stats.type}</h1>
+    </div>
+    {
+      type.map(card => (
+        <div key={card.id} className="card-container">
+          <DragWrapper key={card.id} {...{ card }}>
+            <HandCard inHand card={card} hoverCentered />
+          </DragWrapper>
+        </div>
+      ))
+    }
+  </div>
+);
+
+CardsTabGroup.propTypes = {
+  type: PropTypes.array.isRequired,
+};
+
+export default CardsTabGroup;
