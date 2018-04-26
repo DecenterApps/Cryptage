95bb4f44ae8936d2bbb59daa39c7d301426298cc
Nikola Klipa <klipa.nikola@gmail.com>
2018-04-27 01:25:46 +0200

    Play card with levels done

--- solidity/StateVerifier.v2.sol
+++ solidity/StateVerifier.v2.sol
@@ -52,13 +52,18 @@ contract StateVerifier is StateCodec {
 		//                  all of this is needed for each card
 		// ------------------------------------------------------------------------------
 		// ------------------------------------------------------------------------------
-	    CryptageCards.CardCost memory cardCost = cryptageCards.getCardCost(_move.card);
+	    CryptageCards.CardCost memory cardCost = cryptageCards.getCardCost(_move.card, _move.level);
+	    if (_move.level > 0) {
+	    	CryptageCards.CardCost memory cardCostLevelDown = cryptageCards.getCardCost(_move.card, _move.level-1);
+	    }
         
 		require(_state.developmentLeft > cardCost.dev); 
 		require(_state.funds > cardCost.funds);
 		require(_state.experience >= levels[cardCost.level]);
 		require(_state.locations[_move.location].powerLeft >= cardCost.power); 
 		require(_state.locations[_move.location].spaceLeft >= cardCost.space);
+		require(_move.level < 5);
+		
 
 		// pay costs
 		_state.developmentLeft -= cardCost.dev; 
@@ -67,22 +72,34 @@ contract StateVerifier is StateCodec {
 		_state.locations[_move.location].spaceLeft -= cardCost.space;
 	
 		// get gains
-		CryptageCards.CardGains memory cardGains = cryptageCards.getCardGains(_move.card);
+		CryptageCards.CardGains memory cardGains = cryptageCards.getCardGains(_move.card, _move.level);
+		if (_move.level > 0) {
+	    	CryptageCards.CardGains memory cardGainsLevelDown = cryptageCards.getCardGains(_move.card, _move.level-1);
+	    }
 		// time will be cost if you receive gains after some time
 		if (cardCost.time == 0) {
 			_state.fundsPerBlock += cardGains.funds;
 			_state.developmentLeft += cardGains.dev;
 			_state.locations[_move.location].powerLeft += cardGains.power;
 			_state.locations[_move.location].spaceLeft += cardGains.space;
+			if (_move.level > 0) {
+				// remove level before gains
+				_state.fundsPerBlock -= cardGainsLevelDown.funds;
+				_state.developmentLeft -= cardGainsLevelDown.dev;
+				_state.locations[_move.location].powerLeft -= cardGainsLevelDown.power;
+				_state.locations[_move.location].spaceLeft -= cardGainsLevelDown.space;
+			}
 		}
 		// ------------------------------------------------------------------------------
 		// ------------------------------------------------------------------------------
 
 
 		// now we do all special things for each card
-    	if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.LOCATION)) {
+		// no leveling for location
+    	if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.LOCATION)) {
     		require(_state.locations[_move.location].numberOfCards == 0);
     		_state.locations[_move.location] = Location({
+    		    exists: 1,
 				card: _move.card,
 				numberOfCards: 0,
 				spaceLeft: cardGains.space,
@@ -96,21 +113,45 @@ contract StateVerifier is StateCodec {
 			});
     	}
 
-    	if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.PROJECT)) {
-			require(_state.projects[_move.location].timeLeft == 0);
+    	if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.PROJECT)) {
+			if (_move.level > 0) {
+				bool exists = false;
+				for (uint i=0; i<_state.projects.length; i++) {
+					if (_state.projects[i].card == _move.card && _state.projects[i].level == _move.level-1 && _state.projects[i].timeLeft == 0) {
+						exists = true;
+						_state.projects[i].level++;
+						_state.projects[i].timeLeft = cardCost.time;
+						break;
+					}
+				}
 
-    		_state.projects[_move.location] = Project({
-				card: _move.card,
-				timeLeft: cardCost.time,
-				level: _move.level
-			});
+				require(exists);
+			} else {
+				exists = false;
+				for (i=0; i<_state.projects.length; i++) {
+					if (_state.projects[i].card == 0) {
+			    		_state.projects[i] = Project({
+							card: _move.card,
+							timeLeft: cardCost.time,
+							level: 0
+						});
+						exists = true;
+						break;
+		    		}
+	    		}
+
+	    		require(exists);
+    		}
     	}
 
-    	if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.POWER)) {
-    		bool exists = false;
-    		for(uint i=0; i<_state.locations[_move.location].powers.length; i++) {
-    			if (_state.locations[_move.location].powers[i].card[_move.level] == _move.card) {
+    	if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.POWER)) {
+    		exists = false;
+    		for(i=0; i<_state.locations[_move.location].powers.length; i++) {
+    			if (_state.locations[_move.location].powers[i].card == _move.card) {
+					require(_state.locations[_move.location].powers[i].count[_move.level-1] > 0);
+
     				_state.locations[_move.location].powers[i].count[_move.level] += 1;
+    				_state.locations[_move.location].powers[i].count[_move.level-1] -= 1;
     				exists = true;
     				break;
     			}
@@ -125,19 +166,22 @@ contract StateVerifier is StateCodec {
 
                 uint[5] memory emptyLevels;
 				_state.locations[_move.location].powers[powers.length] = Power({
-					card: emptyLevels,
+					card: _move.card,
 					count: emptyLevels
 				});
-				_state.locations[_move.location].powers[powers.length].card[_move.level] = _move.card;
-				_state.locations[_move.location].powers[powers.length].count[_move.level] = 1;
+
+				_state.locations[_move.location].powers[powers.length].count[0] = 1;
 			}
 		}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.DEV)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.DEV)) {
     		exists = false;
     		for(i=0; i<_state.locations[_move.location].developers.length; i++) {
-    			if (_state.locations[_move.location].developers[i].card[_move.level] == _move.card) {
+    			if (_state.locations[_move.location].developers[i].card == _move.card) {
+    				require(_state.locations[_move.location].developers[i].count[_move.level-1] > 0);
+
     				_state.locations[_move.location].developers[i].count[_move.level] += 1;
+    				_state.locations[_move.location].developers[i].count[_move.level-1] -= 1;
     				exists = true;
     				break;
     			}
@@ -151,28 +195,27 @@ contract StateVerifier is StateCodec {
 				}
 
 				_state.locations[_move.location].developers[developers.length] = Developer({
-					card: emptyLevels,
+					card: _move.card,
 					count: emptyLevels
 				});
-				_state.locations[_move.location].developers[developers.length].card[_move.level] = _move.card;
-				_state.locations[_move.location].developers[developers.length].count[_move.level] = 1;
+				_state.locations[_move.location].developers[developers.length].count[0] = 1;
 			}
     	}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.COMPUTER_CASE)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.COMPUTER_CASE)) {
 			StateCodec.ComputerCase[] memory computerCases = _state.locations[_move.location].computerCases;
 			_state.locations[_move.location].computerCases = new StateCodec.ComputerCase[](computerCases.length + 1);
 			for(i=0; i<computerCases.length; i++) {
 				_state.locations[_move.location].computerCases[i] = computerCases[i];
 			}
 		
-		    uint[computerCaseMinersCount * cpuLevelCount] memory counts;
+		    uint[computerCaseMinersCount * computerCaseMinersLevelCount] memory counts;
 			_state.locations[_move.location].computerCases[computerCases.length] = ComputerCase({
-                counts: counts
+                count: counts
 			});
 		}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.MOUNT_CASE)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.MOUNT_CASE)) {
 			StateCodec.MountCase[] memory mountCases = _state.locations[_move.location].mountCases;
 			_state.locations[_move.location].mountCases = new StateCodec.MountCase[](mountCases.length + 1);
 			for(i=0; i<mountCases.length; i++) {
@@ -184,37 +227,43 @@ contract StateVerifier is StateCodec {
 			});
 		}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.ASIC)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.ASIC)) {
 			require(_state.locations[_move.location].mountCases.length > _move.containerIndex);
+
 			totalCount = 0;
 			for(i=0; i<asicLevelCount; i++) {
 			    totalCount += _state.locations[_move.location].mountCases[_move.containerIndex].asicCount[i];
 			}
 			require(totalCount < 6);
+			require(_move.level == 0 || _state.locations[_move.location].mountCases[_move.containerIndex].asicCount[_move.level-1] > 0);
+
+			if (_move.level > 1) {
+				_state.locations[_move.location].mountCases[_move.containerIndex].asicCount[_move.level-1]--;
+			}
 
 			_state.locations[_move.location].mountCases[_move.containerIndex].asicCount[_move.level]++;
 		}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.RIG_CASE)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.RIG_CASE)) {
 			StateCodec.RigCase[] memory rigCases = _state.locations[_move.location].rigCases;
 			_state.locations[_move.location].rigCases = new StateCodec.RigCase[](rigCases.length + 1);
 			for(i=0; i<rigCases.length; i++) {
 				_state.locations[_move.location].rigCases[i] = rigCases[i];
 			}
 		
-		   uint[rigCaseMinersCount * gpuLevelCount] memory counts2;
+		   uint[rigCaseMinersCount * rigCaseMinersLevelCount] memory counts2;
 			_state.locations[_move.location].rigCases[rigCases.length] = RigCase({
-				counts: counts2
+				count: counts2
 			});
 		}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.MINING)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.MINING)) {
 
 			if (_move.gpuOption == false) {
 				uint pos = computerCaseMiners.length;
 				uint totalCount = 0;
 				for (i=0; i<computerCaseMiners.length; i++) {
-					totalCount += _state.locations[_move.location].computerCases[_move.containerIndex].counts[i];
+					totalCount += _state.locations[_move.location].computerCases[_move.containerIndex].count[i];
 					if (computerCaseMiners[i] == _move.card) {
 						pos = i;
 					}
@@ -222,23 +271,31 @@ contract StateVerifier is StateCodec {
 
 				require(pos < computerCaseMiners.length);
 				require(totalCount < 2);
+				require(_move.level == 0 || _state.locations[_move.location].computerCases[_move.containerIndex].count[pos*(_move.level-1)] > 0);
 				
-				_state.locations[_move.location].computerCases[_move.containerIndex].counts[pos]++;
+				if (_move.level == 0) {
+					_state.locations[_move.location].computerCases[_move.containerIndex].count[pos*(_move.level-1)]++;
+				}
+				_state.locations[_move.location].computerCases[_move.containerIndex].count[pos*_move.level]++;
 			} else {
 
 				pos = rigCaseMiners.length;
 				totalCount = 0;
 				for (i=0; i<rigCaseMiners.length; i++) {
-					totalCount += _state.locations[_move.location].rigCases[_move.containerIndex].counts[i];
+					totalCount += _state.locations[_move.location].rigCases[_move.containerIndex].count[i];
 					if (rigCaseMiners[i] == _move.card) {
-						exists = true;
+						pos = i;
 					}
 				}
 
 				require(pos < rigCaseMiners.length);
 				require(totalCount < 6);
+				require(_move.level == 0 || _state.locations[_move.location].rigCases[_move.containerIndex].count[pos*(_move.level-1)] > 0);
 				
-				_state.locations[_move.location].rigCases[_move.containerIndex].counts[pos]++;
+				if (_move.level == 0) {
+					_state.locations[_move.location].rigCases[_move.containerIndex].count[pos*(_move.level-1)]++;
+				}
+				_state.locations[_move.location].rigCases[_move.containerIndex].count[pos*_move.level]++;
 			}
 		}
     	
@@ -250,7 +307,7 @@ contract StateVerifier is StateCodec {
     	//                  all of this is needed for each card
 		// ------------------------------------------------------------------------------
 		// ------------------------------------------------------------------------------
-		CryptageCards.CardGains memory cardGains = cryptageCards.getCardGains(_move.card);
+		CryptageCards.CardGains memory cardGains = cryptageCards.getCardGains(_move.card, _move.level);
 
 		require(_state.developmentLeft - cardGains.dev >= 0);
 		require(_state.locations[_move.location].powerLeft - cardGains.power >= 0);
@@ -263,14 +320,14 @@ contract StateVerifier is StateCodec {
 		// ------------------------------------------------------------------------------
 		// ------------------------------------------------------------------------------
     	
-    	if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.LOCATION)) {
+    	if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.LOCATION)) {
     		require(_state.locations[_move.location].numberOfCards == 0);
     	}
 
-    	if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.POWER)) {
+    	if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.POWER)) {
     		bool exists = false;
     		for(uint i=0; i<_state.locations[_move.location].powers.length; i++) {
-    			if (_state.locations[_move.location].powers[i].card[_move.level] == _move.card) {
+    			if (_state.locations[_move.location].powers[i].card == _move.card) {
     				exists = true;
     				break;
     			}
@@ -282,10 +339,10 @@ contract StateVerifier is StateCodec {
     		_state.locations[_move.location].powers[i].count[_move.level]--;
 		}
 
-		if (uint(cryptageCards.getCardType(_move.card)) == uint(CryptageCards.CardType.DEV)) {
+		if (uint(cryptageCards.getCardType(_move.card, _move.level)) == uint(CryptageCards.CardType.DEV)) {
     		exists = false;
     		for(i=0; i<_state.locations[_move.location].developers.length; i++) {
-    			if (_state.locations[_move.location].developers[i].card[_move.level] == _move.card) {
+    			if (_state.locations[_move.location].developers[i].card == _move.card) {
     				exists = true;
     				break;
     			}
@@ -315,7 +372,7 @@ contract StateVerifier is StateCodec {
     		}else {
     			_state.projects[i].timeLeft = 0;
     			// add funds for finished project
-    			CryptageCards.CardGains memory projectGains = cryptageCards.getCardGains(_state.projects[i].card);
+    			CryptageCards.CardGains memory projectGains = cryptageCards.getCardGains(_state.projects[i].card, _move.level);
     			_state.funds += projectGains.funds;
     			_state.experience += projectGains.xp;
     		}
