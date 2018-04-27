8e2279f034eef5f5218f780f5168180084a0c5bc
nebojsa.urosevic <nebojsa94urosevic@gmail.com>
2018-04-27 16:53:32 +0200

    Single move contain 42 bits.

--- solidity/MoveDecoder.sol
+++ solidity/MoveDecoder.sol
@@ -4,6 +4,7 @@ contract MoveDecoder {
   struct Move {
     bool shift;
     bool gpuOption;
+    uint level;
     uint location;
     uint containerIndex;
     uint card;
@@ -13,7 +14,7 @@ contract MoveDecoder {
 
   function decode(uint[] _moves) internal pure returns (Move[] memory moves, uint sendBlockNumber) {
     for (uint i = 1; i < 6; i++) {
-      if ((_moves[_moves.length - 1] & ((uint(2) << (215 - 40 * i)) * 0xFFFFFFFFFF)) == ((uint(2) << (215 - 40 * i)) * 0xFFFFFFFFFF)) {
+      if ((_moves[_moves.length - 1] & ((uint(2) << (215 - 42 * i)) * 0x2FFFFFFFFFF)) == ((uint(2) << (215 - 42 * i)) * 0x2FFFFFFFFFF)) {
         uint offset = i;
       }
     }
@@ -28,19 +29,20 @@ contract MoveDecoder {
           continue;
         }
 
-        if ((_moves[i] & ((uint(2) << (215 - 40 * j)) * 0xFFFFFFFFFF)) == ((uint(2) << (215 - 40 * j)) * 0xFFFFFFFFFF)) {
+        if ((_moves[i] & ((uint(2) << (215 - 42 * j)) * 0xFFFFFFFFFF)) == ((uint(2) << (215 - 42 * j)) * 0xFFFFFFFFFF)) {
           break;
         }
 
-        uint blockDifference = (_moves[i] & ((uint(2) << (215 - 40 * j))) * 65535) / (uint(2) << (215 - 40 * j));
+        uint blockDifference = (_moves[i] & ((uint(2) << (214 - 42 * j))) * 65535) / (uint(2) << (214 - 42 * j));
         moves[i * 6 + j - 2] = Move({
-          shift: (_moves[i] & (uint(2) << (255 - 40 * j))) > 0,
-          gpuOption: (_moves[i] & (uint(2) << (254 - 40 * j))) > 0,
-          location: (_moves[i] & ((uint(2) << (251 - 40 * j))) * 7) / (uint(2) << (251 - 40 * j)),
-          containerIndex: (_moves[i] & ((uint(2) << (243 - 40 * j))) * 255) / (uint(2) << (243 - 40 * j)),
-          card: (_moves[i] & ((uint(2) << (232 - 40 * j))) * 2047) / (uint(2) << (232 - 40 * j)),
-          blockDifference: blockDifference,
-          blockNumber: blockNumber += blockDifference
+          shift: (_moves[i] & (uint(2) << (255 - 42 * j))) > 0,
+          gpuOption: (_moves[i] & (uint(2) << (254 - 42 * j))) > 0,
+          level: (_moves[i] & ((uint(2) << (251 - 42 * j))) * 7) / (uint(2) << (251 - 42 * j)),
+          location: (_moves[i] & ((uint(2) << (248 - 42 * j))) * 7) / (uint(2) << (248 - 42 * j)),
+          containerIndex: (_moves[i] & ((uint(2) << (241 - 42 * j))) * 127) / (uint(2) << (241 - 42 * j)),
+          card: (_moves[i] & ((uint(2) << (230 - 42 * j))) * 2047) / (uint(2) << (230 - 42 * j)),
+          blockNumber: blockNumber += blockDifference,
+          blockDifference: blockDifference
           });
       }
     }
