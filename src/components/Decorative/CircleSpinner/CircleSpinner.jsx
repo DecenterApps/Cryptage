e05571c406a3e7331dbabff593bb5c92d7ac8b73
astankovic <aleksa.stankovic@berkeley.edu>
2018-05-10 11:20:37 +0200

    Implement responsive design for app wrapper, locations.

--- src/components/Decorative/CircleSpinner/CircleSpinner.jsx
+++ src/components/Decorative/CircleSpinner/CircleSpinner.jsx
@@ -4,7 +4,7 @@ import './CircleSpinner.scss';
 
 const CircleSpinner = () => (
   <div className="spinner-wrapper">
-    <svg viewBox="0 0 32 32" width="32" height="32">
+    <svg viewBox="0 0 32 32" width="32">
       <circle id="spinner" cx="16" cy="16" r="14" fill="none" />
     </svg>
   </div>
