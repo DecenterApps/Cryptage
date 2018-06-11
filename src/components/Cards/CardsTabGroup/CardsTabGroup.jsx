7e2f827358311fe900a491cdc2f384f2281f88bd
sterlu <sterlu.sd@gmail.com>
2018-06-11 17:26:38 +0200

    Bottom cards area redesign

--- src/components/Cards/CardsTabGroup/CardsTabGroup.jsx
+++ src/components/Cards/CardsTabGroup/CardsTabGroup.jsx
@@ -4,25 +4,28 @@ import HandCard from '../HandCard/HandCard';
 import DragWrapper from '../../DragWrapper/DragWrapper';
 import { typeToPluralMapping } from '../../../constants/mappings';
 
-const CardsTabGroup = ({ type }) => (
+const CardsTabGroup = ({ cards, title }) => (
   <div className="card-type-wrapper">
     <div className="card-type-title-wrapper">
-      <h1 className="card-type-title">{ typeToPluralMapping(type[0].stats.type) }</h1>
+      <h1 className="card-type-title">{ typeToPluralMapping(title) }</h1>
     </div>
-    {
-      type.map(card => (
-        <div key={card.id} className="card-container">
-          <DragWrapper key={card.id} {...{ card }}>
-            <HandCard inHand card={card} hoverCentered />
-          </DragWrapper>
-        </div>
-      ))
-    }
+    <div className="card-type-inner-wrapper">
+      {
+        cards.map(card => (
+          <div key={card.id} className="card-container">
+            <DragWrapper key={card.id} {...{ card }}>
+              <HandCard inHand card={card} hoverCentered />
+            </DragWrapper>
+          </div>
+        ))
+      }
+    </div>
   </div>
 );
 
 CardsTabGroup.propTypes = {
-  type: PropTypes.array.isRequired,
+  cards: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default CardsTabGroup;
