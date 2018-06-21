bc7241cf33c02b9b7c937e2fb7f430e14f76c794
astankovic <aleksa.stankovic@berkeley.edu>
2018-06-21 10:04:08 +0200

    Remove hash from router.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import App from './components/App/App';
 
 export const RoutesWrapper = ({ store }) => (
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={App} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
 
