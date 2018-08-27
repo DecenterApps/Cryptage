17e1fc7dcee3c6cdf085fc10dfc06fd5704b9dcd
Aleksandar Cakalic <aleksandar.cakalic@gmail.com>
2018-08-27 13:09:24 +0200

    Fix typo

--- src/components/Gameplay/GameplayHeader/GameplayHeader.jsx
+++ src/components/Gameplay/GameplayHeader/GameplayHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { formattedNumber } from '../../services/utils';
+import { formattedNumber } from '../../../services/utils';
 
 import './GameplayHeader.scss';
 
